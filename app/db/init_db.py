7bb34c8f712c28a17c1171bcd14af7df01ee48ff
Hasan Aliyev <hasan@onneks.com>
2020-07-29 14:35:38 +0400

    modified dev settings for sqlite

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import sessionmaker
 from app.core.settings import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+if settings.config.get("APP_ENV_SETTINGS") == "DEVELOPMENT":
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+elif settings.config.get("APP_ENV_SETTINGS") == "PRODUCTION":
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
